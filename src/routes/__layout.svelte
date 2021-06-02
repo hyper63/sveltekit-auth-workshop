ef9bab48d79471e69ef2ac1cab32a5c273b74426
Tom Wilson <twilson63@gmail.com>
2021-06-02 20:18:10 +0000

    added protect endpoint example

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<script context="module">
+  export async function load({ page, session }) {
+    if (/^\/admin\/(.*)/.test(page.path) && session.user === null) {
+      return { redirect: "/", status: 302 };
+    }
+    return { props: {} };
+  }
+</script>
+
+<slot />

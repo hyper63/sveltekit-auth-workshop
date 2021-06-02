4d21d308e8133162f42d5d6ca38693c4b002f4c6
Tom Wilson <twilson63@gmail.com>
2021-06-02 19:43:12 +0000

    added login endpoint

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,3 +2,6 @@
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
+<a href="/login">
+  <button>Login using Github</button>
+</a>
---
+++ src/routes/login.js
@@ -0,0 +1,13 @@
+const target = 'https://github.com/login/oauth/authorize'
+const clientId = import.meta.env.VITE_CLIENT_ID
+
+export async function get(request) {
+  const sessionId = '1234'
+
+  return {
+    status: 302,
+    headers: {
+      location: `${target}?client_id=${clientId}&state=${sessionId}`
+    }
+  }
+}

94d1600a4a0f881bf9227f78df5b0b5bd5ec9b25
Tom Wilson <twilson63@gmail.com>
2022-01-03 14:28:14 -0500

    Update hooks.js

--- src/hooks.js
+++ src/hooks.js
@@ -1,7 +1,7 @@
 import cookie from 'cookie'
 
 export async function handle({ request, resolve }) {
-  const cookies = cookie.parse(request.headers.cookie || { user: null })
+  const cookies = Object.assign({user: null}, cookie.parse(request.headers.cookie || "") )
 
   // do some stuff before
   request.locals.user = cookies.user
